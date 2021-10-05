b899e58f1b6c922661c806d43b222908d4c21cb6
JADeveloper <jesusdeveloper404@gmail.com>
2021-10-05 08:23:20 -0500

    Agregando configuraciones de vue - vue-router

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $blogs = Blog::all();
+        
+        return response()->json($blogs, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $blog = Blog::create($request->post());
+
+        return response()->json([
+            'blog' => $blog
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Blog $blog)
+    {
+        return response()->json($blog, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        $blog->fill($request->post());
+
+        return response()->json([
+            'blog' => $blog
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Blog $blog)
+    {
+        $blog->delete();
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,29 @@
 require('./bootstrap');
+
+import vue from 'vue'
+
+window.Vue = vue;
+
+import App from './components/App.vue';
+
+// Importando axios
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+// Importamos Vuerouter
+import VueRouter from 'vue-router';
+import { routes } from './routes';
+import Vue from 'vue';
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routes
+});
+
+const app = new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
